de6a8d21e9387d8bf662bb89732be375e20bda07
Valery-Holovina <valery.holovina09@gmail.com>
2025-07-28 17:04:36 +0300

    Initial Commit

---
+++ MessengerApp1.0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MessengerApp1.0
+//
+//  Created by Valery on 28.07.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MessengerApp1.0/MessengerApp1_0App.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerApp1_0App.swift
+//  MessengerApp1.0
+//
+//  Created by Valery on 28.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct MessengerApp1_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
