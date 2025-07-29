99291431b06b0f4690b18836f34a5caacb4acb7b
Valery-Holovina <valery.holovina09@gmail.com>
2025-07-29 13:11:53 +0300

    Adding toolbar for inbox

---
+++ MessengerApp1.0/Preview Content/Core/Inbox/InboxView.swift
@@ -0,0 +1,44 @@
+//
+//  InboxView.swift
+//  MessengerApp1.0
+//
+//  Created by Valery on 29.07.2025.
+//
+
+import SwiftUI
+
+struct InboxView: View {
+    var body: some View {
+        NavigationStack{
+            ScrollView{
+                
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    HStack{
+                        Image(systemName: "person.circle.fill")
+                        Text("Chats")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                    }
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        print("show new message view")
+                    } label: {
+                        Image(systemName: "square.and.pencil.circle.fill")
+                            .resizable()
+                            .frame(width: 32, height: 32)
+                            .foregroundStyle(.black, Color(.systemGray5))
+                        
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    InboxView()
+}
